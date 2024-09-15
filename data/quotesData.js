0847a0820969c37c6dcdc4906b25930c3132e063
Brian Zhou <87774511+brianlzhou@users.noreply.github.com>
2024-09-15 15:16:43 -0400

    Update quotesData.js

--- data/quotesData.js
+++ data/quotesData.js
@@ -1,46 +1,18 @@
 export const quotes = [
   {
-    author: 'Steve Jobs',
-    quote: `Your time is limited, so don't waste it living someone else's life.`,
+    author: 'Paul Graham',
+    quote: `Don't ignore your dreams; don't work too much; say what you think; cultivate friendships; be happy.`,
   },
   {
-    author: 'Rafael Nadal',
-    quote: `If, if, if... doesn't exist.`,
+    author: 'Reed Hastings',
+    quote: `Stone Age. Bronze Age. Iron Age. We define entire epics of humanity by the technology they use.`,
   },
   {
-    author: 'Rafael Nadal',
-    quote: `The glory is being happy. The glory is not winning here or winning there. The glory is enjoying practicing, enjoy every day, enjoying to work hard, trying to be a better player than before.`,
+    author: 'John Green',
+    quote: `The world may be broken, but hope is not crazy.`,
   },
   {
-    author: 'Rafael Nadal',
-    quote: `If you don't lose, you cannot enjoy the victories. So I have to accept both things.`,
-  },
-  {
-    author: 'Roger Federer',
-    quote: `When you do something best in life, you don't really want to give that up, and for me it's tennis.`,
-  },
-  {
-    author: 'Roger Federer',
-    quote: `There's no way around hard work. Embrace it.`,
-  },
-  {
-    author: 'Roger Federer',
-    quote: `You have to put in the hours because there's always something which you can improve.`,
-  },
-  {
-    author: 'Roger Federer',
-    quote: `A man who wins is a man who thinks he can.`,
-  },
-  {
-    author: 'Roger Federer',
-    quote: `When you're good at something, make that everything.`,
-  },
-  {
-    author: 'Roger Federer',
-    quote: `I've practiced so much.`,
-  },
-  {
-    author: 'Michael Jordan',
-    quote: `limits, like fears, are often just illusions`,
+    author: 'Otto Von Bismarck',
+    quote: `When you want to fool the world, tell the truth.`,
   },
 ]
